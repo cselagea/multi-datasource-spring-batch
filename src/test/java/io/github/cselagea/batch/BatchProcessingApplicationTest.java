724102042a653a38f48b1d4bc2c23a0f7adf40d3
Cosmin Selagea-Popov <cosmin.sel@gmail.com>
2020-10-05 21:59:32 -0400

    Use @DynamicPropertySource instead of implementing ApplicationContextInitializer to set data source properties in test

--- src/test/java/io/github/cselagea/batch/BatchProcessingApplicationTest.java
+++ src/test/java/io/github/cselagea/batch/BatchProcessingApplicationTest.java
@@ -1,17 +1,15 @@
 package io.github.cselagea.batch;
 
-import io.github.cselagea.batch.BatchProcessingApplicationTest.DataSourceConfigurationPropertiesInitializer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.util.TestPropertyValues;
-import org.springframework.context.ApplicationContextInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.testcontainers.containers.JdbcDatabaseContainer;
 import org.testcontainers.containers.MSSQLServerContainer;
@@ -29,7 +27,7 @@ import static org.assertj.core.api.Assertions.tuple;
 
 @SpringBatchTest
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = BatchProcessingApplication.class, initializers = DataSourceConfigurationPropertiesInitializer.class)
+@ContextConfiguration(classes = BatchProcessingApplication.class)
 @Testcontainers
 class BatchProcessingApplicationTest {
 
@@ -37,14 +35,14 @@ class BatchProcessingApplicationTest {
             = DockerImageName.parse("mcr.microsoft.com/mssql/server:2019-CU6-ubuntu-16.04");
 
     @Container
-    private static final JdbcDatabaseContainer batchJobSpecificSqlServerContainer
-            = new MSSQLServerContainer(sqlServerImageName)
+    private static final JdbcDatabaseContainer<?> batchJobSpecificSqlServerContainer
+            = new MSSQLServerContainer<>(sqlServerImageName)
             .acceptLicense()
             .withInitScript("batch-job-schema-mssql.sql");
 
     @Container
-    private static final JdbcDatabaseContainer batchFrameworkSqlServerContainer
-            = new MSSQLServerContainer(sqlServerImageName)
+    private static final JdbcDatabaseContainer<?> batchFrameworkSqlServerContainer
+            = new MSSQLServerContainer<>(sqlServerImageName)
             .acceptLicense()
             .withInitScript("org/springframework/batch/core/schema-sqlserver.sql");
 
@@ -57,6 +55,16 @@ class BatchProcessingApplicationTest {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
+
+    @DynamicPropertySource
+    static void registerDataSourceProperties(DynamicPropertyRegistry registry) {
+        registry.add(BATCH_FRAMEWORK_DATA_SOURCE_CONFIG_PREFIX + ".url", batchFrameworkSqlServerContainer::getJdbcUrl);
+        registry.add(BATCH_FRAMEWORK_DATA_SOURCE_CONFIG_PREFIX + ".username", batchFrameworkSqlServerContainer::getUsername);
+        registry.add(BATCH_FRAMEWORK_DATA_SOURCE_CONFIG_PREFIX + ".password", batchFrameworkSqlServerContainer::getPassword);
+        registry.add(BATCH_JOB_DATA_SOURCE_CONFIG_PREFIX + ".url", batchJobSpecificSqlServerContainer::getJdbcUrl);
+        registry.add(BATCH_JOB_DATA_SOURCE_CONFIG_PREFIX + ".username", batchJobSpecificSqlServerContainer::getUsername);
+        registry.add(BATCH_JOB_DATA_SOURCE_CONFIG_PREFIX + ".password", batchJobSpecificSqlServerContainer::getPassword);
+    }
 
     @Test
     void jobShouldPersistUppercaseNames() throws Exception {
@@ -78,23 +86,6 @@ class BatchProcessingApplicationTest {
                                       tuple("JANE", "DOE"),
                                       tuple("JOHN", "DOE")
                               );
-    }
-
-    static class DataSourceConfigurationPropertiesInitializer
-            implements ApplicationContextInitializer<ConfigurableApplicationContext> {
-
-        @Override
-        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
-            TestPropertyValues.of(
-                    BATCH_FRAMEWORK_DATA_SOURCE_CONFIG_PREFIX + ".url=" + batchFrameworkSqlServerContainer.getJdbcUrl(),
-                    BATCH_FRAMEWORK_DATA_SOURCE_CONFIG_PREFIX + ".username=" + batchFrameworkSqlServerContainer.getUsername(),
-                    BATCH_FRAMEWORK_DATA_SOURCE_CONFIG_PREFIX + ".password=" + batchFrameworkSqlServerContainer.getPassword(),
-                    BATCH_JOB_DATA_SOURCE_CONFIG_PREFIX + ".url=" + batchJobSpecificSqlServerContainer.getJdbcUrl(),
-                    BATCH_JOB_DATA_SOURCE_CONFIG_PREFIX + ".username=" + batchJobSpecificSqlServerContainer.getUsername(),
-                    BATCH_JOB_DATA_SOURCE_CONFIG_PREFIX + ".password=" + batchJobSpecificSqlServerContainer.getPassword()
-            ).applyTo(configurableApplicationContext.getEnvironment());
-        }
-
     }
 
 }
